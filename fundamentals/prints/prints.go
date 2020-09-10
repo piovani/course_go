f65cf87f50f6e40b6e46329d454ab7df30ab01d5
Allison Piovani <apiovani25@gmail.com>
2020-09-10 19:56:59 -0300

    prints

---
+++ fundamentals/prints/prints.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Mesma")
+	fmt.Print("Linha.")
+
+	x := 3.141516
+
+	// fmt.Println("Teste: " + x)
+
+	xs := fmt.Sprint(x)
+	fmt.Println("Teste: " + xs)
+
+	
+}
