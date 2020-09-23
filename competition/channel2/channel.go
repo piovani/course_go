60d184295d6e8815376d2f51e0602a8118f23ca9
Allison Piovani <apiovani25@gmail.com>
2020-09-23 19:57:33 -0300

    channel 2

---
+++ competition/channel2/channel.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doisTresQuatroVezes(base int, c chan int) {
+	time.Sleep(time.Second)
+	c <- 2 * base
+
+	time.Sleep(time.Second)
+	c <- 3 * base
+
+	time.Sleep(time.Second)
+	c <- 4 * base
+}
+
+func main() {
+	c := make(chan int)
+	go doisTresQuatroVezes(2, c)
+
+	a, b := <-c, <-c
+
+	fmt.Printf("A: %d ; B: %d\n", a, b)
+
+	fmt.Println(<-c)
+}
